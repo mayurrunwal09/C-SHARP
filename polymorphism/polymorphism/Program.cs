7fb95bac7b186696fff02024056c9fca4a77ef1c
mayurrunwal09 <126644458+mayurrunwal09@users.noreply.github.com>
2023-10-20 19:19:30 +0530

    Add files via upload

---
+++ polymorphism/polymorphism/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphism
+{
+
+    public class Math
+    {
+        public virtual void add(int a,int b,int c)
+        {
+            
+            Console.WriteLine("The addition is "+(a+b+c)); 
+        }
+      public virtual void show1()
+        {
+            Console.WriteLine("HELLO");
+        }
+    }
+    public class show : Math
+    {
+        public override void add(int a, int b, int c)
+        {
+
+            Console.WriteLine("The addition is " + (a + b + c));
+        }
+        public override void show1()
+        {
+            Console.WriteLine("WORLD");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            show s = new show();
+            s.add(10,20,30);
+            s.show1();
+
+
+           
+          
+            
+        }
+    }
+}
