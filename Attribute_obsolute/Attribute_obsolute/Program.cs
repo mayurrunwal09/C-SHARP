3bdcd8718ad48d4a9992d5bfa5dbf468a7158059
mayurrunwal09 <126644458+mayurrunwal09@users.noreply.github.com>
2023-10-20 19:08:30 +0530

    Add files via upload

---
+++ Attribute_obsolute/Attribute_obsolute/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Attribute_obsolute
+{
+    public class MyClass
+    {
+        [Obsolete("Don't use OldMethod, use NewMethod instead", false)]
+
+        static void OldMethod()
+        {
+            Console.WriteLine("It is the old method");
+        }
+        static void NewMethod()
+        {
+            Console.WriteLine("It is the new method");
+        }
+        public static void Main()
+        {
+            OldMethod();
+        }
+    }
+
+}
