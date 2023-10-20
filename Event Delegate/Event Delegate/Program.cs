422b3c5254fc2e4f69c8829d19362c5ea2b141f4
mayurrunwal09 <126644458+mayurrunwal09@users.noreply.github.com>
2023-10-20 19:13:57 +0530

    Add files via upload

---
+++ Event Delegate/Event Delegate/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event_Delegate
+{
+    internal class Program
+    {
+        public static void bl_ProcessCompleted()
+        {
+            Console.WriteLine("Process Completed!");
+        }
+    }
+
+    public delegate void Notify();  // delegate
+
+    public class ProcessBusinessLogic
+    {
+        public event Notify ProcessCompleted; // event
+
+        public void StartProcess()
+        {
+            Console.WriteLine("Process Started!");
+            // some code here..
+            OnProcessCompleted();
+        }
+
+
+        protected virtual void OnProcessCompleted()
+        {
+            ProcessCompleted?.Invoke();
+        }
+        static void Main(string[] args)
+        {
+            ProcessBusinessLogic bl = new ProcessBusinessLogic();
+            bl.ProcessCompleted += bl.ProcessCompleted; // register with an event
+            bl.StartProcess();
+        }
+    }
+}
+ 
