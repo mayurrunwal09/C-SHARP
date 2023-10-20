08fa71d5b7092b383f2c05e9755fc8e41330850b
mayurrunwal09 <126644458+mayurrunwal09@users.noreply.github.com>
2023-10-20 19:09:05 +0530

    Add files via upload

---
+++ composition/composition/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace composition
+{
+    internal class Program
+    {
+        public class emp
+        {
+            public int eid { get; set; }
+            public string name { get; set; }
+
+            public int age { get; set; }
+           
+
+    }
+        public class company
+        {
+            public string name { get; set; }
+            public string city { get; set; }
+
+        }
+        static void Main(string[] args)
+        {
+
+            emp e = new emp();
+            e.eid = 101;
+            e.name = "Abc";
+            e.age = 24;
+            Console.WriteLine(e.eid);
+            Console.WriteLine(e.name);
+            Console.WriteLine(e.age);
+
+
+
+            company c = new company();
+            c.name = "Triveni";
+            c.city = "surat";
+            Console.WriteLine(c.name);
+            Console.WriteLine(c.city);
+
+
+          
+            
+        }
+    }
+}
