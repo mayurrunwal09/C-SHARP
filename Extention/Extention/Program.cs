34b57f7dfe6115367630dba2d87372b1fd4e792d
mayurrunwal09 <126644458+mayurrunwal09@users.noreply.github.com>
2023-10-20 19:15:20 +0530

    Add files via upload

---
+++ Extention/Extention/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;using System.Collections.Generic;using System.Linq;using System.Text;using System.Threading.Tasks;namespace Extension_Method{    public static class Ext    {      public static bool check(this int i,int n)
+        {
+            return i > n;
+        }
+
+        static void Main(string[] args)        {
+
+            int i = 109;            bool res = i.check(100);            Console.WriteLine("Result is "+res);        }    }}
