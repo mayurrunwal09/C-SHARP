579291b7bdba54a7835f76d3509e6ae74822ee0a
mayurrunwal09 <126644458+mayurrunwal09@users.noreply.github.com>
2023-10-20 19:05:05 +0530

    Add files via upload

---
+++ Accesspecfier/Accesspecfier/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accesspecfier
+{
+    internal class Program
+    {
+        public class Animal
+        {
+            public string name;
+            public Animal(string name)
+            {
+                this.name = name;
+            }
+            public void makesount()
+            {
+                Console.WriteLine($ "{name} make some sound: ");
+            }
+
+            class dog : Animal
+            {
+
+            }
+        }
+        }
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
