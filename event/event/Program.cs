551167cee4904fb047735cd6c55c4ed23cc8bb61
mayurrunwal09 <126644458+mayurrunwal09@users.noreply.github.com>
2023-10-20 19:13:26 +0530

    Add files via upload

---
+++ event/event/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+public class program
+
+{
+    public delegate void nofity();
+    public static void bl_ProcessCompleted()
+    {
+        Console.WriteLine("Process Completed!");
+    }
+  
+    public class logic
+    {
+        public event nofity processcompleted;
+
+        public void startprocess()
+        {
+            Console.WriteLine("process started");
+
+            onprocesscomplete();
+        }
+
+        public virtual void onprocesscomplete()
+        {
+            processcompleted?.Invoke();
+        }
+
+    }
+    public static void Main(string[] srgs)
+    {
+
+        logic logic = new logic();
+        logic.processcompleted += bl_ProcessCompleted;
+        logic.startprocess();
+
+    }
+}
