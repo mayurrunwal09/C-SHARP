af55354f3e6fe9f60946eb2c7494541451b6fa9a
mayurrunwal09 <126644458+mayurrunwal09@users.noreply.github.com>
2023-10-20 19:06:52 +0530

    Add files via upload

---
+++ Arraylist/Aggregation/Aggregation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aggregation
+{
+    internal class Program
+    {
+
+        public string Empname { get; set; }
+        public int id { get; set; }
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Arraylist/Arraylist/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Collections
+{
+    internal class Arraylist
+    {
+        static void Main(string[] args)
+        {
+
+
+            var arlist = new ArrayList();
+            arlist.Add("Mayur");
+            arlist.Add(1);
+            arlist.Add(3);
+            arlist.Add(true);
+            arlist.Add(false);
+            arlist.Add(6.5f);
+
+            Console.WriteLine(arlist.Count); // count the arraylist
+            Console.WriteLine("Elements of array");
+            arlist.Remove(1);  
+            arlist.Remove(true);
+            arlist.Add(100);
+
+
+
+
+            for (int i = 0; i < arlist.Count; i++)
+            {
+
+                Console.WriteLine(arlist[i]);
+
+            }
+        }
+    }
+}
