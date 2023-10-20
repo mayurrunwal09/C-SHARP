6be998217e6e1c9d80d80777c54f74cd8ad34c3d
mayurrunwal09 <126644458+mayurrunwal09@users.noreply.github.com>
2023-10-20 19:17:19 +0530

    Add files via upload

---
+++ forloop/forloop/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forloop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // for loop
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine("Value of i: {0}", i);
+            }
+            
+
+            
+               // while loop
+            int j = 0;
+            while (j < 20)
+            {
+                Console.WriteLine("value of j is {0} ", j);
+                j++;
+            } 
+            
+
+            // do while loop
+            int m = 0;
+            do
+            {
+                Console.WriteLine(m);
+                m++;
+            }
+            while (m <= 20);
+
+        }
+    }
+}
