fc02577e9ba9fbaf7e655d00944a1e2184f6513b
mayurrunwal09 <126644458+mayurrunwal09@users.noreply.github.com>
2023-10-20 19:14:22 +0530

    Add files via upload

---
+++ Exception handing/Exception handing/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exception
+{
+    public class Program
+    {
+        public class CustomException : Exception
+        {
+            public CustomException(string per) : base(per)
+            {
+
+            }
+        }
+
+        static void check(int per)
+        {
+            if (per > 35)
+            {
+                throw new CustomException("Pass!!!!!!!!!!!!!!!!");
+            }
+            Console.WriteLine("Fail");
+        }
+
+
+       
+        static void Main(string[] args)
+        {
+            try
+            {
+                check(36);
+            }
+            catch (CustomException e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                Console.WriteLine("All the best");
+            }
+
+
+        }
+    }
+}
