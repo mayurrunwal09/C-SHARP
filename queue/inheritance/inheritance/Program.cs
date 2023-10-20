a89f61084aa172c33ad0fd5f070fa9ebc26610b1
mayurrunwal09 <126644458+mayurrunwal09@users.noreply.github.com>
2023-10-20 19:20:49 +0530

    Add files via upload

---
+++ queue/inheritance/inheritance/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance
+{
+    public class Program
+    {
+        private class Person
+        {
+           public string Name { get; set; }
+            public string lastname { get; set; }
+
+            public string getname()
+            {
+                return Name + "" + lastname;
+            }
+        }
+        class emp : Person
+        {
+            public int Empid { get; set; }
+            public string Compname { get; set; }
+        }
+        class emp1 : emp
+        {
+            public long contact { get; set; }
+
+        }
+
+        public static void Main(string[] args)
+        {
+            emp e = new emp();
+            e.Name = "Mayur";
+            e.lastname = "Runwal";
+            e.Empid = 101;
+            e.Compname = "Triveni";
+            Console.WriteLine("The name is " + e.Name);
+            Console.WriteLine("Last name is "+e.lastname);
+            Console.WriteLine("The id of emp is " + e.Empid);
+            Console.WriteLine("The name of company is " + e.Compname);
+
+            emp1 e1 = new emp1();
+            e1.contact = 9923428304;
+            Console.WriteLine(e1.contact);
+
+        }
+    }
+}
+
---
+++ queue/queue/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> q = new Queue<int>();
+            q.Enqueue(1);
+            q.Enqueue(2);
+            q.Enqueue(3);
+            q.Enqueue(4);
+            q.Enqueue(5);
+
+            Console.WriteLine("the total enqueue:" + q.Count);
+
+
+            foreach (var u in q)
+                Console.WriteLine(u);
+
+            Queue<string> s = new Queue<string>();
+            s.Enqueue("hello");
+            s.Enqueue("dushd");
+
+            Console.WriteLine(s.Count);
+
+            while(s.Count > 0)
+            {
+                Console.WriteLine(s.Dequeue());
+                Console.WriteLine(s.Count);
+            }
+
+           
+
+        }    
+    }
+}
