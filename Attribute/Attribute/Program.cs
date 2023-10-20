3232287b1ed1247402e33b85cba51f7cb5060e3e
mayurrunwal09 <126644458+mayurrunwal09@users.noreply.github.com>
2023-10-20 19:08:02 +0530

    Add files via upload

---
+++ Attribute/Attribute/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Attribute ///////conditional Attribute...
+{
+   public class myclass
+    {
+        [Conditional("DEBUG")]
+        public static void message(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+    class test
+    {
+        static void function1()
+        {
+            myclass.message("message in function 1");
+            function2();
+        }
+        static void function2()
+        {
+            myclass.message("message in function 2");
+        }
+        public static void Main(string[] args)
+        {
+            myclass.message("message in main");
+            function1();
+           
+        }
+    }
+
+}
