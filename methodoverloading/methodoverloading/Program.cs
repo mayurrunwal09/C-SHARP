ffda472ed5c13ee0cd2b37cbf296a16377539829
mayurrunwal09 <126644458+mayurrunwal09@users.noreply.github.com>
2023-10-20 19:18:33 +0530

    Add files via upload

---
+++ methodoverloading/methodoverloading/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodoverloading
+{
+
+    public class Math
+    {
+        public void add(int a,int b)
+        {
+            
+            Console.WriteLine(a + b);
+        }
+        public void add(int a,int b,int c)
+        {
+            Console.WriteLine(a + b + c);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Math m = new Math();
+            m.add(10,20);
+            m.add(10,20,30);    
+
+        }
+    }
+}
