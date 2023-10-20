3aa41c1991122296cfcd64cf09c3e5a35de84ea4
mayurrunwal09 <126644458+mayurrunwal09@users.noreply.github.com>
2023-10-20 19:05:42 +0530

    Add files via upload

---
+++ Action delegate/Action delegate/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Action_delegate
+{
+    internal class Program
+    {
+       public delegate void Print(int val);
+       
+        static void ConsolePrint(int i)
+       {
+           Console.WriteLine(i);
+       }
+
+
+        public delegate void show(string message);
+        static void Mine(string message)
+        {
+            Console.WriteLine("Hello "+message);
+
+        }
+
+        static void Main(string[] args)
+        {
+
+
+
+           Print prnt = ConsolePrint;
+           prnt(10);
+
+            show s = Mine;
+            s("Mayur");
+        }
+
+        
+        
+
+
+    }
+}
